8bd648e9d59f09cff8f203cee39cc4715730db97
Anton Chebotaev <anton.chebotaev@gmail.com>
2015-04-21 15:09:01 +0300

    more stuff

---
+++ .pallet/config.clj
@@ -0,0 +1,6 @@
+(defpallet
+  ;; You can specify a default service to be used
+  ;; :default-service :vmfest
+  ;; you can specify global data in the :environment key here
+  ;; :environment {:proxy "http://192.168.1.37:3128"}
+)
